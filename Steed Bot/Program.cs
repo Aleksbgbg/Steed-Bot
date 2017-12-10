ea82921a9a9dbf9db3d2b614ac185412dbcf0bc4
Aleksbgbg <kozichka01@abv.bg>
2017-12-10 17:13:07 +0000

    Add project files.

---
+++ Steed Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Steed_Bot
+{
+    using System;
+    using static System.Console;
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
---
+++ Steed Bot/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Steed Bot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Steed Bot")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("43fc5b44-f018-4bb5-af98-8ff4b0d43c26")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
